4dc81926407dd0c1b7404cf07906d0a5fe25db3d
Fredirecko <avspedaliere@gmail.com>
2020-02-22 15:11:18 -0800

    updated Quizzer program with save method and finished the startQuiz mehtod

--- Quizzer.py
+++ Quizzer.py
@@ -7,7 +7,9 @@
 
 
 class Quizzer:
-    """ Class for inputting questions and answer pairs for future quizzing. """
+    """ Class that stores all the major methods of the program.
+    Methods include inputting questions and answer pairs for future quizzing, starting the quiz, saving the quiz,
+    load a quiz, and exiting the program. """
 
     question_bank = {}
     answer_bank = {}
@@ -22,6 +24,7 @@ class Quizzer:
         self.question = ""
         self.answer = ""
         self.q = 1
+        self.o = 0
 
         print("")
         print("-----New Quiz------")
@@ -42,17 +45,18 @@ class Quizzer:
             print("")
             print("Enter question number " + str(self.q) + " below.")
             self.question = input()
-            self.new_Q = {(self.q): self.question}
+            self.new_Q = {(self.o): self.question}
 
             print("")
             print("Enter answer for question " + str(self.q) + " below.")
             self.answer = input()
-            self.new_A = {(self.q): self.answer}
+            self.new_A = {(self.o): self.answer}
 
             #updates dictionary with inputs
             self.question_bank.update(self.new_Q)
             self.answer_bank.update(self.new_A)
             self.q += 1
+            self.o += 1
 
             self.y = self.anotherQuestion()
 
@@ -81,24 +85,45 @@ class Quizzer:
                     return False
 
     def startQuiz(self):
-
+        print("")
+        print("")
         print("Quiz Name: " + self.quiz_name)
         print("Description: " + self.description)
         print("Name: " + self.first_name + " " + self.last_name)
         print("")
+        print("")
 
-        for i in self.question_bank:
+        length = len(self.question_bank)
+
+        for i in range(length):
             print("Problem: " + self.question_bank[i])
             print("")
-            print("Enter 1 to show answer")
-            proceed = int(input())
-            if proceed == 1:
-                for j in self.answer_bank:
+            print("Enter 1 to show answer.")
+            print("")
+
+            try:
+                proceed = int(input())
+            except ValueError:
+                print("")
+                print("Invalid option.")
+                print("Enter 1 to show answer.")
+                print("")
+                continue
+            else:
+                if proceed == 1:
                     print("")
-                    print("Answer: " + self.answer_bank[j])
+                    print("Answer: " + self.answer_bank[i])
+                    print("")
 
     def saveQuiz(self):
-        pass
+        print("")
+        print("What would you like to name your file?")
+        fileName = input()
+
+        newQuiz = open(fileName, "a")
+        newQuiz.write("test")
+        newQuiz.close()
+        #newQuiz.write("Quiz Name: " + self.quiz_name "\n Description: " + self.description "\n Created by: " + self.first_name + self.last_name + "\n")
 
     def loadQuiz(self):
         pass
@@ -113,12 +138,17 @@ class Quizzer:
         pass
 
 class Start(Quizzer):
-    """ Class for menu items and methods. """
+    """ Child class of Quizzer for mainMenu. """
 
     def mainMenu(self):
+        print("")
+        print("")
+        print("")
         print("*************************************")
         print("        Welcome to Quizzer!          ")
         print("*************************************")
+        print("")
+        print("")
         print("")
 
         while True:
@@ -127,9 +157,9 @@ class Start(Quizzer):
             print("")
             print("      Enter 1 to create new quiz     ")#new quiz will ask for name, quiz name, quiz description
             print("      Enter 2 to begin quiz          ") #start quiz
+            print("      Enter 3 to save current quiz   ") #save quiz to txt file
             print("")
-            print("      Enter 3 to load quiz           ") #load file with saved quiz from txt file
-            print("      Enter 4 to save current quiz   ") #save quiz to txt file
+            print("      Enter 4 to load quiz           ") #load file with saved quiz from txt file
             print("      Enter 5 to exit                ") # exit program
             print("")
             print("-------------------------------------")
@@ -144,7 +174,10 @@ class Start(Quizzer):
             elif self.selection == 2:
                 self.startQuiz()
             elif self.selection == 3:
-                print("success")
+                self.saveQuiz()
+                print("")
+                print("**** quiz successfully saved ****")
+                print("")
             elif self.selection == 4:
                 print("success")
             elif self.selection == 5:
